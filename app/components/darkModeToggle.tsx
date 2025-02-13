cba94b849cdb9c6ecad11c7df6a869ca5c1d078f
Harsha107 <harshadeep107@gmail.com>
2025-02-13 16:37:59 +0400

    Update darkModeToggle.tsx

--- app/components/darkModeToggle.tsx
+++ app/components/darkModeToggle.tsx
@@ -9,9 +9,9 @@ export default function DarkModeToggle() {
     return (
         <button
             onClick={toggleTheme}
-            className="fixed top-5 right-5 border-2 p-2 rounded-full bg-hray-200 dark:bg-gray-200 transition-all"
+            className="fixed top-5 right-5 border-2 p-2 rounded-full bg-black dark:bg-gray-200 transition-all duration-300"
         >
-            {darkMode ? <Sun className="text-yellow-400"/> : <Moon className="text-gray-800"/>}
+            {darkMode ? <Sun className="text-yellow-400"/> : <Moon className="text-white"/>}
         </button>
     )
 }
