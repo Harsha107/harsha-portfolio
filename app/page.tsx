33244c7403d7a355aba0a82f04927300c74bcedf
Harsha107 <harshadeep107@gmail.com>
2025-02-13 12:26:47 +0400

    v0.00

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <h1>Welcome to my personal website</h1>
+    <main className="mt-20 mx-40 mb-10 bg-white">
+      <Header />
+      <h1 className="text-4xl text-center">Hello, world!</h1>
+    </main>
   )
 }
+
