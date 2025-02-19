e049fadc1e0ecde973472ed3eda5d6a032d7d58a
Harsha107 <harshadeep107@gmail.com>
2025-02-19 13:02:55 +0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { ThemeProvider } from "./components/theme";
 import DarkModeToggle from "./components/darkModeToggle";
@@ -32,6 +33,7 @@ export default function RootLayout({
                     <DarkModeToggle />
                     <Navbar />
                     {children}
+                    <Analytics />
                 </ThemeProvider>
             </body>
         </html>
