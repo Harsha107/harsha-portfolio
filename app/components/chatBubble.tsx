692daec4c2f24f2b413c359044a30015ceb8a1f0
Harshadeep Kommareddi <116015254+Harsha107@users.noreply.github.com>
2025-03-25 12:50:03 +0400

    Update chatBubble.tsx

--- app/components/chatBubble.tsx
+++ app/components/chatBubble.tsx
@@ -40,7 +40,7 @@ export default function ChatBubble() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/chat', {
+            const response = await fetch('https://harsha-portfolio-p0vq.onrender.com/api/chat', {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ messages: [...messages, userMessage] }),
